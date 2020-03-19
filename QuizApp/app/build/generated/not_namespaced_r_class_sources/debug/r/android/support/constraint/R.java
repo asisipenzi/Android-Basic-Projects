ed8360e87b1304ac0a389ad2fe6bb94c80f47555
Andrew <sisipenzia@gmail.com>
2020-03-19 20:12:08 +0200

    Added the submit button, checks if all questions answered

--- QuizApp/app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/constraint/R.java
+++ QuizApp/app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f07002d;
-        public static final int end = 0x7f070042;
-        public static final int gone = 0x7f070049;
-        public static final int invisible = 0x7f070053;
-        public static final int left = 0x7f070055;
-        public static final int packed = 0x7f070063;
-        public static final int parent = 0x7f070064;
-        public static final int percent = 0x7f070066;
-        public static final int right = 0x7f07006a;
-        public static final int spread = 0x7f070082;
-        public static final int spread_inside = 0x7f070083;
-        public static final int start = 0x7f070088;
-        public static final int top = 0x7f070097;
-        public static final int wrap = 0x7f07009d;
+        public static final int bottom = 0x7f070031;
+        public static final int end = 0x7f070046;
+        public static final int gone = 0x7f07004d;
+        public static final int invisible = 0x7f070057;
+        public static final int left = 0x7f070059;
+        public static final int packed = 0x7f070067;
+        public static final int parent = 0x7f070068;
+        public static final int percent = 0x7f07006a;
+        public static final int right = 0x7f07006f;
+        public static final int spread = 0x7f070087;
+        public static final int spread_inside = 0x7f070088;
+        public static final int start = 0x7f07008d;
+        public static final int top = 0x7f07009d;
+        public static final int wrap = 0x7f0700a3;
     }
     public static final class styleable {
         private styleable() {}
--- QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +16,135 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onRadioButtonClicked(View view) {
+    /**
+     * This method checks if all the questions are answered
+     *
+     * @param answerSeven
+     * @param answerEight
+     * @param answerNine
+     * @param answerTen
+     * @param rightAns
+     */
+    public void incompleteAnswers(String answerSeven, String answerEight, String answerNine,
+                                  String answerTen, int rightAns) {
 
+        if (answerSeven == null || answerEight == null || answerNine == null || answerTen == null) {
+            Toast.makeText(this, "Please answer all the question.", Toast.LENGTH_LONG);
+
+        } else {
+            Toast.makeText(this, "You got " + rightAns + "Correct", Toast.LENGTH_LONG);
+        }
+
+    }
+
+    /**
+     * This method checks if all the questions are answered
+     */
+    public void submitAnswers(View view) {
+        int correctAnswers = 0;
+
+        //Getting values from the RadioButtons and TextView
+        boolean checked = ((RadioButton) view).isChecked();
+        Log.v("MainActivity", "variable is: " + checked);
+        TextView questionSeven = (TextView) findViewById(R.id.answerSeven);
+        String question7 = questionSeven.getText().toString();
+
+        TextView questionEight = (TextView) findViewById(R.id.answerEight);
+        String question8 = questionEight.getText().toString();
+
+        TextView questionNine = (TextView) findViewById(R.id.answerNine);
+        String question9 = questionNine.getText().toString();
+
+        TextView questionTen = (TextView) findViewById(R.id.answerTen);
+        String question10 = questionTen.getText().toString();
+
+
+        //Question 1
+        switch (view.getId()) {
+            case R.id.answer_2:
+                if (checked)
+                    correctAnswers += 1;
+                break;
+            case R.id.answer_3:
+                if (checked)
+                    break;
+        }
+
+        //Question 2
+        switch (view.getId()) {
+            case R.id.answer_2:
+                if (checked)
+                    correctAnswers += 1;
+                break;
+            case R.id.answer_3:
+                if (checked)
+                    break;
+        }
+
+        //Question 3
+        switch (view.getId()) {
+            case R.id.answer_2:
+                if (checked)
+                    correctAnswers += 1;
+                break;
+            case R.id.answer_3:
+                if (checked)
+                    break;
+        }
+
+        //Question 4
+        switch (view.getId()) {
+            case R.id.answer_2:
+                if (checked)
+                    correctAnswers += 1;
+                break;
+            case R.id.answer_3:
+                if (checked)
+                    break;
+        }
+
+
+        //Question 5
+        switch (view.getId()) {
+            case R.id.answer_2:
+                if (checked)
+                    correctAnswers += 1;
+                break;
+            case R.id.answer_3:
+                if (checked)
+                    break;
+        }
+
+        //Question 6
+        switch (view.getId()) {
+            case R.id.answer_2:
+                if (checked)
+                    correctAnswers += 1;
+                break;
+            case R.id.answer_3:
+                if (checked)
+                    break;
+        }
+
+        //Question 7
+        if (questionSeven.getText().toString() == "soll")
+            correctAnswers += 1;
+
+        //Question 8
+        if (questionEight.getText().toString() == "meinem")
+            correctAnswers += 1;
+
+        //Question 9
+        if (questionNine.getText().toString() == "ihn")
+            correctAnswers += 1;
+
+        //Question 10
+        if (questionTen.getText().toString() == "so daa")
+            correctAnswers += 1;
+
+        incompleteAnswers(question7, question8, question9, question10, correctAnswers);
+    }
+
+    public void resetQuestions(View view) {
     }
 }

a3c2e376e93b8cbf82041d43c81de59b03db29fd
Andrew <sisipenzia@gmail.com>
2020-03-31 16:38:54 +0200

    Completed MusicApp Code

--- MusicApp/app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ MusicApp/app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button songDetail = (Button) findViewById(R.id.song_detail);
+        Button musicStore = (Button) findViewById(R.id.music_store);
+        Button musicList = (Button) findViewById(R.id.music_list);
+
+        songDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent songDetailsIntent = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivity(songDetailsIntent);
+            }
+        });
+
+
+        musicStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent musicStoreIntent = new Intent(MainActivity.this, StoreActivity.class);
+                startActivity(musicStoreIntent);
+            }
+        });
+
+        musicList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent musicListIntent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(musicListIntent);
+            }
+        });
     }
 }
--- MusicApp/app/src/main/java/com/example/android/musicapp/StoreActivity.java
+++ MusicApp/app/src/main/java/com/example/android/musicapp/StoreActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StoreActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class StoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        Button nowPlaying = (Button) findViewById(R.id.playing_song);
+        nowPlaying.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nowPlayingIntent = new Intent(StoreActivity.this, MainActivity.class);
+                startActivity(nowPlayingIntent);
+            }
+        });
     }
 }
